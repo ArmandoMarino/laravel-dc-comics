c460dc56204ae8d02d400955a39d4faad11bca04
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-28 18:33:31 +0100

    css update

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,11 +5,11 @@
 {{-- HERO INCLUDE --}}
 @section('content')
 <!-- BLACKBOARD PRODUCTS LIST -->
-<section id="contents-comics">
-    <div class="blackboard">
+<section id="contents-space">
+    <div class="blackboard text-white">
         @foreach ($comics as $comic)
-        <div class="product-card">
-            <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+        <div class="col-12 col-md-6 col-lg-2">
+            <img class="img-fluid" src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
             <p>{{ $comic->title }}</p>
         </div>
         @endforeach
