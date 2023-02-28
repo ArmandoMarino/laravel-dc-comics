369a3a7926472b946245d9e82793cfc5c54e2f17
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-28 18:00:19 +0100

    bootstrap cnv header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,23 +3,29 @@
   $links = config('menu'); 
 @endphp
 
-<header class="container">
-    <figure id="logo-header">
-        <a href="{{ route('home') }}">
-            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC_Logo">
-        </a>
-    </figure>
-    <nav>
-        <ul>
-            {{-- @foreach ($links as $link) --}}
-            <li>
-                <a class="nav-link @if (Route::is('home')) active @endif" href="{{ route('home') }}">Home</a>
-            </li>
-            <li>
-                <a class="nav-link @if (Route::is('comics.index')) active @endif" href="{{ route('comics.index') }}">Comics</a>
-            </li>
-            {{-- @endforeach --}}
-
-        </ul>
-    </nav>
+<header>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <figure id="logo-header">
+                <a href="{{ route('home') }}">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC_Logo">
+                </a>
+            </figure>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li>
+                    <a class="nav-link @if (Route::is('home')) active @endif" href="{{ route('home') }}">Home</a>
+                </li>
+                <li>
+                    <a class="nav-link @if (Route::is('comics.index')) active @endif" href="{{ route('comics.index') }}">Comics</a>
+                </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 </header>
+
+
