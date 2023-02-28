cfd0368d4bb039a9a62ff31484f13fabc25ff253
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-28 16:28:29 +0100

    migration ready

---
+++ database/migrations/2023_02_28_145920_create_comics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price', 50);
+            $table->string('series', 20);
+            $table->date('sale_date');
+            $table->string('type', 20);
+            $table->text('artists');
+            $table->text('writers');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
